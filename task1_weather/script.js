55bb23f8ba98dc7cfd2c6e55f01049021776a848
asaminiko <chebakova29@gmail.com>
2025-03-26 14:25:52 +0200

    created task 1

---
+++ task1_weather/script.js
@@ -0,0 +1,29 @@
+const LAT = 50.4674
+const LON = 30.4828
+const API_KEY = '9e93d1a8af0cad2bf51a472ff0b0f91b'
+const getAsync = async (lat, lon, appid, units) => {
+  try {
+    const params = new URLSearchParams({ lat, lon, appid, units })
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?${params}`
+    )
+    if (!response.ok) throw new Error(`Помилка: ${response.status}`)
+    const weather = await response.json()
+    widget.textContent = `Температура: ${weather.main.temp}°C`
+  } catch (error) {
+    console.error(`Помилка: ${error.message}`)
+  }
+}
+
+let parent = document.getElementById('parent')
+let widget = document.createElement('p')
+let button = document.createElement('button')
+button.id = 'myBtn'
+widget.id = 'widget'
+button.textContent = 'Оновити дані'
+parent.appendChild(widget)
+parent.appendChild(button)
+
+button.addEventListener('click', function () {
+  getAsync(LAT, LON, API_KEY, 'metric')
+})
